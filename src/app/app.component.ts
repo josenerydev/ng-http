6b0302f8d3a49e5534e610897100b51e622c1c49
josenerydev <josenerydev@gmail.com>
2021-04-24 12:27:06 -0300

    256. GETting Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,23 +11,33 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     this.http.post(
-      'http://localhost:5000/posts', 
+      'http://localhost:5000/posts',
       postData)
-        .subscribe(responseData => {
-          console.log(responseData);
-        });
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get('http://localhost:5000/posts')
+      .subscribe(posts => {
+        console.log(posts);
+      });
   }
 }
