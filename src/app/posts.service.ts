5d0327cb2589a213009d5b11cbf472208f0e7d95
josenerydev <josenerydev@gmail.com>
2021-04-25 21:38:51 -0300

    262. Services & Componentes Working Together

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -21,7 +21,7 @@ export class PostsService {
   }
 
   fetchPosts() {
-    this.http.get<{ [key: string]: Post }>('https://ng-complete-guide-62aad-default-rtdb.firebaseio.com/posts.json')
+    return this.http.get<{ [key: string]: Post }>('https://ng-complete-guide-62aad-default-rtdb.firebaseio.com/posts.json')
       .pipe(map(responseData => {
         const postsArray: Post[] = [];
         for (const key in responseData) {
@@ -30,8 +30,6 @@ export class PostsService {
           }
         }
         return postsArray;
-      }))
-      .subscribe(posts => {
-      });
+      }));
   }
 }
